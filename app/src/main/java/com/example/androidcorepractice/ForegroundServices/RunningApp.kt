8d673938d5b3b55595a736d218014f452cd631d4
pr-Dutta <89duttapranjal@gmail.com>
2024-09-27 12:50:42 +0530

    new learning

--- app/src/main/java/com/example/androidcorepractice/ForegroundServices/RunningApp.kt
+++ app/src/main/java/com/example/androidcorepractice/ForegroundServices/RunningApp.kt
@@ -6,21 +6,21 @@ import android.app.NotificationManager
 import android.content.Context
 import android.os.Build
 
-class RunningApp: Application() {
-
-    override fun onCreate() {
-        super.onCreate()
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(
-                "running_channel",
-                "Running Notifications",
-                NotificationManager.IMPORTANCE_HIGH
-            )
-            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-        }
-    }
-}
+//class RunningApp: Application() {
+//
+//    override fun onCreate() {
+//        super.onCreate()
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+//            val channel = NotificationChannel(
+//                "running_channel",
+//                "Running Notifications",
+//                NotificationManager.IMPORTANCE_HIGH
+//            )
+//            val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+//            notificationManager.createNotificationChannel(channel)
+//        }
+//    }
+//}
 
 
 
