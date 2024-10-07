1e2c77d233feba05ef405a31e2b371295fe4f385
pr-Dutta <89duttapranjal@gmail.com>
2024-10-07 12:12:36 +0530

    new commit

--- app/src/main/java/com/example/androidcorepractice/WorkManagerFullGuide/DownloadWorker.kt
+++ app/src/main/java/com/example/androidcorepractice/WorkManagerFullGuide/DownloadWorker.kt
@@ -4,14 +4,14 @@ import android.content.Context
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 
-// - ()05-10-2024
+// - (05-10-2024)
 // Now we will create a so called worker: worker is basically a class that
 // defines our work manager task.
 // This would potentially also be something very large, to do
 // you always not use work manager, You can use it for very big file
 // to download in the background reliably.
 
-// We want ot use workManager to get up with coroutines
+// We want to use workManager to get up with coroutines
 class DownloadWorker(
     // This two will contain more specific information about the worker
     private val context: Context,
@@ -26,7 +26,19 @@ class DownloadWorker(
     // retried later (that's also possible with work manager)
 
     // In this function we have to download the image using the API
+
+    // - (06-10-2024)
+    // WorkManager contains a behaviour of showing a easy notification
+    // since in the can if we have a long running task it will
+    // execute it in a foreground service, and when we want to show
+    // notification then we need to create a notification channel
+    // that is usually done in an application class
     override suspend fun doWork(): Result {
         TODO("Not yet implemented")
     }
 }
+
+
+
+
+
