32cbbe4f1b66beb6de3a4b163f4362537b5f3dba
pr-Dutta <89duttapranjal@gmail.com>
2024-10-10 18:33:02 +0530

    new learning

--- app/src/main/java/com/example/androidcorepractice/WorkManagerFullGuide/WorkerParams.kt
+++ app/src/main/java/com/example/androidcorepractice/WorkManagerFullGuide/WorkerParams.kt
@@ -1,7 +1,7 @@
 package com.example.androidcorepractice.WorkManagerFullGuide
 
-object WorkerParams {
-    const val ERROR = "errorMsg"
-    const val IMAGE_URI = "imageUri"
-    const val FILTER_URI = "filterUri"
-}
+//object WorkerParams {
+//    const val ERROR = "errorMsg"
+//    const val IMAGE_URI = "imageUri"
+//    const val FILTER_URI = "filterUri"
+//}
