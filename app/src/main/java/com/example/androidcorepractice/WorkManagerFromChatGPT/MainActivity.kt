7c2e0a471c90782ac692dd9b7bd2d4d97b5b29c5
pr-Dutta <89duttapranjal@gmail.com>
2024-10-18 11:03:25 +0530

    new learning

--- app/src/main/java/com/example/androidcorepractice/WorkManagerFromChatGPT/MainActivity.kt
+++ app/src/main/java/com/example/androidcorepractice/WorkManagerFromChatGPT/MainActivity.kt
@@ -5,13 +5,51 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.work.Constraints
+import androidx.work.NetworkType
 import androidx.work.OneTimeWorkRequest
+import androidx.work.PeriodicWorkRequest
 import androidx.work.WorkManager
 import com.example.androidcorepractice.R
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Schedule the work
+
+        // - (17 -10-2024)
+        // If you want to add constraints like "only run when connected
+        // to Wi-Fi", you can add them in the same place where you're
+        // scheduling the work.
+
+        // Constraints.Builder()
+        //This line creates an instance of the Constraints.Builder class,
+        // which allows you to specify conditions or constraints that
+        // must be met before a WorkManager task can be executed.
+
+        // This method is called on the Constraints.Builder object and
+        // specifies that the task (in this case, a Worker) should only
+        // run when the device is connected to a network. NetworkType.CONNECTED
+        // means that the work will run when the device is connected to
+        // any type of network (either Wi-Fi or cellular data).
+
+        // .setRequiresBatteryNotLow(true)
+        // This sets another constraint: the work will only run when the
+        // device's battery is not low. In other words, the task will be
+        // postponed if the battery is running low. This is useful for
+        // saving battery power on tasks that can be deferred.
+
+        // .build()
+        // This call finalizes the creation of the Constraints object
+        // based on the specified conditions (network connected and
+        // battery not low). After calling build(), the constraints
+        // are ready to be used.
+        val constraints = Constraints.Builder()
+            .setRequiredNetworkType(NetworkType.CONNECTED)
+            .setRequiresBatteryNotLow(true)
+            .build()
 
         // - (16-10-2024)
         // The code you've provided is used to schedule a one-time
@@ -20,18 +58,53 @@ class MainActivity : AppCompatActivity() {
         // main app thread (like sending data to a server or doing
         // long-running work).
 
-        // Schedule the work
-
         // OneTimeWorkRequest.Builder(MyWorker::class.java)
         // OneTimeWorkRequest: This is a type of task that runs once.
         // It can be scheduled to run immediately or at a specific
         // time in the future.
-        val workRequest = OneTimeWorkRequest.Builder(MyWorker::class.java).build()
+
+        // - (17-10-2024)
+        // .setConstraints(constraints)
+        // This associates the Constraints (created earlier) with the
+        // OneTimeWorkRequest. It ensures that the background task will
+        // only be executed when the specified conditions (network connected
+        // and battery not low) are met.
+
+        // .build()
+        // This finalizes the creation of the OneTimeWorkRequest.
+        // After this, the request is ready to be passed to WorkManager
+        // to schedule the background task.
+//        val workRequest = OneTimeWorkRequest.Builder(MyWorker::class.java)
+//            .setConstraints(constraints)
+//            .build()
         // .Builder(MyWorker::class.java): This is how you create the
         // request. You specify the task (or worker) that will run.
         // In this case, MyWorker is a class you created that defines
         // the task to be done. It extends a class called Worker, which
         // is where you write the actual code for the background task.
+
+        // - (17-10-2024)
+        // PeriodicWorkRequest
+        // This is a class from the Android WorkManager library.
+        // PeriodicWorkRequest is used to define work that should
+        // happen periodically (for example, every few hours, or daily).
+        // Unlike OneTimeWorkRequest (which is for work that runs only
+        // once), PeriodicWorkRequest is for recurring tasks.
+
+        // Here, the Builder class is being called to configure and
+        // create a PeriodicWorkRequest.
+
+        // 1
+        // This is the interval for how often the periodic work should be triggered.
+        // In this case, the work will repeat every 1 unit of time
+        // specified in the next argument (TimeUnit.HOURS).
+
+        // TimeUnit.HOURS
+        // TimeUnit is an enum in Java that represents time units (such
+        // as seconds, minutes, hours, etc.).
+        // TimeUnit.HOURS means the time unit is in hours, so the
+        // periodic work will be triggered every 1 hour.
+        val periodicWorkRequest = PeriodicWorkRequest.Builder(MyWorker::class.java, 1, TimeUnit.HOURS).build()
 
         WorkManager.getInstance(applicationContext).enqueue(workRequest)
         // WorkManager: This is the Android system's way of handling
